0f882394c1c1ce542294595258d49dd1a06ad77c
Soumil <soumilgad@gmail.com>
2023-08-10 21:24:03 -0700

    set up question answering service

--- app.py
+++ app.py
@@ -8,3 +8,9 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 st.title("Playing around with LangChain")
 prompt = st.text_input('Enter prompt here')
+
+llm = OpenAI(temperature=0.9, max_tokens=100)
+
+if prompt:
+    resp = llm(prompt)
+    st.write(resp)
