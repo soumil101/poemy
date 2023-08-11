6872f33f1af3a160bc92599aff8a626170780739
Soumil <soumilgad@gmail.com>
2023-08-10 21:16:55 -0700

    set up streamlit app

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import os
 import openai
 
+import streamlit as st
+from langchain.llms import OpenAI
+
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-print(openai.api_key)
+st.title("Playing around with LangChain")
+prompt = st.text_input('Enter prompt here')
